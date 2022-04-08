864cedf87e6e8b7ea3d7421cf072ba6b626d150e
UO232627 <UO232627@uniovi.es>
2022-04-08 10:05:09 +0200

    SDI-2122-301-9.3-Mensajes y alertas

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -16,9 +16,14 @@ module.exports = function (app, usersRepository){
     }
 
     usersRepository.insertUser(user).then(userId => {
-      res.redirect("/users/login");
+      res.redirect("/users/login" + '?message=Nuevo usuario registrado.' +
+          "&messageType=alert-info"
+      );
     }).catch(error => {
-      res.send("Error al insertar el usuario");
+      res.redirect("/users/signup" +
+          "?message=Se ha producido un error al registrar el usuario." +
+          "&messageType=alert-danger"
+      )
     });
   });
 
@@ -39,7 +44,10 @@ module.exports = function (app, usersRepository){
     usersRepository.findUser(filter, options).then(user => {
       if (user == null){
         req.session.user = null;
-        res.send("Usuario no identificado");
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto" +
+            "&messageType=alert-danger"
+        );
       }
       else{
         req.session.user = user.email;
@@ -47,7 +55,10 @@ module.exports = function (app, usersRepository){
       }
     }).catch(error => {
       req.session.user = null;
-      res.send("Se ha producido un error al buscar el usuario: " + error)
+      res.redirect("/users/login" +
+          "?message=Email o password incorrecto" +
+          "&messageType=alert-danger"
+      )
     })
   });
 

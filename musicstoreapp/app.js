9870b6b320fb5c5a7e05463c7c2b1eaeac6eb68a
Alejandro Álvarez Solís <UO232627@uniovi.es>
2022-04-24 11:58:27 +0200

    SDI-2122-301-10.3-SW Cliente JQuery + AJAX.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -19,6 +19,14 @@ var authorsRouter = require('./routes/authors');
 var app = express();
 let crypto = require('crypto');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  next();
+});
+
 let expressSession = require('express-session');
 app.use(expressSession({
   secret: 'abcdeefg',
